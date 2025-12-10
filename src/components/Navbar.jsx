87f7fd31fd663e33aefd5aeaed6ea93b31d32b58
tgzgondoz <tgzgondozz@gmail.com>
2025-12-10 16:49:29 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='bg-white '>Navbar</div>
+    <div className='bg-white py-3 shadow-2xl'>
+          <div className='max-w-6xl max-auto flex flex justify-between items-center'>
+            {/* Logo */}
+            <div className=''>
+                <Link to='/' className='font-bold text-3xl'><span className='text-red-500 font-serif'>B</span><h1>lazingTek</h1></Link>
+            </div>
+            {/* Nav Links */}
+            </div>
+
+          </div>
+       
   )
 }
 

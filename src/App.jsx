0f01490f513712a69fc8f3b27a6c9f3ba289108d
tgzgondoz <tgzgondozz@gmail.com>
2025-12-10 17:18:05 +0200

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Contact from './pages/Contact.jsx'
-import Navbar from './components/Navbar.jsx'
-import About from './pages/about.jsx'
+
 import Services from './pages/Services.jsx'
 
 const App = () => {
@@ -12,7 +11,6 @@ const App = () => {
      <Routes>
        <Route path='/' element={<Home/>}/>
        <Route path='/contact' element={<Contact/>}/>
-       <Route path='/about' element={<About/>}/>
        <Route path='/services' element={<Services/>}/>
      </Routes>
     </BrowserRouter>

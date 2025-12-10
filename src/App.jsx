a86486856b84726656c07a2e2d6ef7174e11db16
tgzgondoz <tgzgondozz@gmail.com>
2025-12-10 15:53:02 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import{ BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/about.jsx'
+import Contact from './pages/Contact.jsx'
+import Navbar from './components/Navbar.jsx'
 
 const App = () => {
   return (
-    <div className='text-red-500'> hey hallo </div>
+    <BrowserRouter>
+    <Navbar/>
+     <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/about' element={<About/>}/>
+       <Route path='/contact' element={<Contact/>}/>
+     </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-blue-500 text-white p-4'>Navbar</div>
+  )
+}
+
+export default Navbar
